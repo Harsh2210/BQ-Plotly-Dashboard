30dd47e2fdac9218cbfc037d19fcf7f9434c1deb
Harsh2297 <harshjanyani22@gmail.com>
2020-09-30 14:12:11 -0400

    Update app.py

--- Plotly Dashboard/app.py
+++ Plotly Dashboard/app.py
@@ -15,9 +15,8 @@ import os
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
-server = Flask(__name__)
-server.secret_key = os.environ.get('secret_key', 'secret')
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 ##############################
 #Data Preparation
